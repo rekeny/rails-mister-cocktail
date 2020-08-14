f37e4e7ddd696fceee176eb5ab41007ebe1d7f6c
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-14 12:55:36 +0100

    some styling

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: { case_sensitive: false }
 end
